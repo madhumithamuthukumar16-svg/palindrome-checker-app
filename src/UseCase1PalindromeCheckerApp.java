19d1e3b4d6e4608dfccb842c858ce2686a8f0639
madhumithamuthukumar16-svg <madhumithamuthukumar16@gmail.com>
2026-03-13 09:55:35 +0530

    UC1:Palindrome Checker App

---
+++ src/UseCase1PalindromeCheckerApp.java
@@ -0,0 +1,19 @@
+public class UseCase1PalindromeCheckerApp {
+
+    // Main method – entry point of the Java application
+    public static void main(String[] args) {
+
+        // Display Welcome Message
+        System.out.println("==================================");
+        System.out.println(" Welcome to Palindrome Checker App ");
+        System.out.println("==================================");
+
+        // Display Application Details
+        System.out.println("Application Name : Palindrome Checker");
+        System.out.println("Version          : 1.0");
+
+        // Inform user about next step
+        System.out.println("\nApplication started successfully.");
+        System.out.println("Proceeding to palindrome validation...");
+    }
+}
